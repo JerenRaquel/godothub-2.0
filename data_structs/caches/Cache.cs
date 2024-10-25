d58a89161ac76f52dffc350b5897fe3f1947f539
Jeren Raquel <jeraquel101@gmail.com>
2024-10-25 16:17:57 -0700

    basic write checks

--- data_structs/caches/Cache.cs
+++ data_structs/caches/Cache.cs
@@ -1,13 +1,8 @@
 public abstract class Cache
 {
-    private bool _isDirty = false;
+    protected bool _isDirty = false;
 
     public abstract bool LoadData();
     public abstract bool WriteData();
     public abstract void ForceWrite();
-
-    private void _ParseJsonData()
-    {
-
-    }
 }
--- data_structs/caches/ProjectCache.cs
+++ data_structs/caches/ProjectCache.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using Godot;
+
 public partial class ProjectCache : Cache
 {
+    private Dictionary<string, ProjectDataState> _projects;
+
     public override bool LoadData()
     {
         return false;
@@ -7,7 +12,18 @@ public partial class ProjectCache : Cache
 
     public override bool WriteData()
     {
-        return false;
+        // Check if dirty, no point to write if not
+        if (!this._isDirty) { return true; }
+
+        // If there's no projects, no point to write
+        if (this._projects.Count == 0) { return true; }
+
+        foreach (KeyValuePair<string, ProjectDataState> entry in this._projects)
+        {
+            // Write
+        }
+
+        return true;
     }
 
     public override void ForceWrite()
