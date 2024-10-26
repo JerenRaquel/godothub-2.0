64ed09305d112895936dd88d6a7b5f6107095eda
Jeren Raquel <jeraquel101@gmail.com>
2024-10-26 15:27:53 -0700

    Create ColorScheme.cs

---
+++ resources/ColorScheme.cs
@@ -0,0 +1,15 @@
+public static class ColorTheme
+{
+    public static readonly string BaseBlue = "478cbf";
+    public static readonly string HighlightBlue = "3dbde0";
+    public static readonly string Unknown = "ffff70";
+    public static readonly string Stable = "6aff7c";
+    public static readonly string ReleaseCandidate = "41a6b5";
+    public static readonly string Beta = "24acf2";
+    public static readonly string Dev = "9d5277";
+    public static readonly string Compat = "24acf2";
+    public static readonly string Mobile = "9d5277";
+    public static readonly string Forward = "6aff7c";
+
+    public static string BBCodeColor(this string str, string colorCode) => $"[color='{colorCode}']{str}[/color]";
+}
