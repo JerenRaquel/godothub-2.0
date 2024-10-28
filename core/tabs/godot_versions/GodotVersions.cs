5b36577eae18b8e0fdbe8b87c398a0135bb70c41
Jeren Raquel <jeraquel101@gmail.com>
2024-10-27 21:02:49 -0700

    filedialog + control shield integration

--- core/tabs/godot_versions/GodotVersions.cs
+++ core/tabs/godot_versions/GodotVersions.cs
@@ -5,14 +5,25 @@ public partial class GodotVersions : PanelContainer
 {
     [Export] private PackedScene card;
 
+    private Button _locateButton;
     private GridContainer _cardGrid;
+    private FileDialog _fileDialog;
+
+    public override void _ExitTree()
+    {
+        _locateButton.Pressed -= OnLocatePressed;
+        _fileDialog.VisibilityChanged -= OnFileDialogVisibiltyChanged;
+    }
 
     public override void _Ready()
     {
+        _locateButton = GetNode<Button>("%LocateButton");
+        _locateButton.Pressed += OnLocatePressed;
         _cardGrid = GetNode<GridContainer>("%CardViewContainer");
+        _fileDialog = GetNode<FileDialog>("%FileDialog");
+        _fileDialog.VisibilityChanged += OnFileDialogVisibiltyChanged;
 
-        AddCard("4.5", "Dev", false);
-        AddCard("5.4", "Beta", true);
+        _fileDialog.Hide();
     }
 
     private void AddCard(string version, string build, bool isCSharp)
@@ -20,5 +31,17 @@ public partial class GodotVersions : PanelContainer
         Card cardInstance = card.Instantiate<Card>();
         _cardGrid.AddChild(cardInstance);
         cardInstance.SetData(version, build, isCSharp);
+    }
+
+    private void OnLocatePressed() => _fileDialog.Show();
+
+    private void OnFileDialogVisibiltyChanged()
+    {
+        if (ControlShield.Instance == null) return;
+
+        if (_fileDialog.Visible)
+            ControlShield.Instance.Show();
+        else
+            ControlShield.Instance.Hide();
     }
 }
