7257004638cc306ef91c6217b1d2b978c27b66af
Jeren Raquel <jeraquel101@gmail.com>
2024-10-28 00:04:50 -0700

    filedialog puts selected path into line edit

--- core/windows/locate_godot_window/LocateGodotWindow.cs
+++ core/windows/locate_godot_window/LocateGodotWindow.cs
@@ -5,10 +5,45 @@ public partial class LocateGodotWindow : WindowBase
 {
     [Export] private FileDialog _fileDialog;
 
+    private LineEdit _pathLineEdit;
+    private Button _chooseLocationButton;
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        _chooseLocationButton.Pressed -= OnChooseLocationPressed;
+        _fileDialog.VisibilityChanged -= OnFileDialogVisibilityChanged;
+        _fileDialog.FileSelected -= OnFileDialogFileSelected;
+    }
+
     public override void _Ready()
     {
         base._Ready();
 
+        _fileDialog.VisibilityChanged += OnFileDialogVisibilityChanged;
+        _fileDialog.FileSelected += OnFileDialogFileSelected;
+        _pathLineEdit = GetNode<LineEdit>("%PathLineEdit");
+        _chooseLocationButton = GetNode<Button>("%ChooseLocationButton");
+        _chooseLocationButton.Pressed += OnChooseLocationPressed;
+
         DisplayError("Missing Data");
+    }
+
+    private void OnChooseLocationPressed()
+    {
+        ControlShield.Instance.Show();
+        _fileDialog.Show();
+    }
+
+    private void OnFileDialogVisibilityChanged()
+    {
+        if (_fileDialog.Visible || ControlShield.Instance == null) return;
+
+        ControlShield.Instance.Hide();
+    }
+
+    private void OnFileDialogFileSelected(string path)
+    {
+        _pathLineEdit.Text = path;
     }
 }
