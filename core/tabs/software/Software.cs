58d6c7cb80d33d62a6727ebe4c05a7c9147161d7
Jeren Raquel <jeraquel101@gmail.com>
2024-10-31 20:59:29 -0700

    created UI linking code for Software Tab

--- core/tabs/software/Software.cs
+++ core/tabs/software/Software.cs
@@ -3,5 +3,31 @@ using System;
 
 public partial class Software : TabBase
 {
+    [Export] private PackedScene entryScene;
 
+    private Button _locateButton;
+    private Button _editButton;
+    private Button _launchButton;
+    private LineEdit _filterLineEdit;
+    private CheckBox _orderCheckBox;
+    private Button _deleteButton;
+
+    public override void _Ready()
+    {
+        _locateButton = GetNode<Button>("%LocateButton");
+        _editButton = GetNode<Button>("%EditButton");
+        _launchButton = GetNode<Button>("%LaunchButton");
+        _filterLineEdit = GetNode<LineEdit>("%FilterLineEdit");
+        _orderCheckBox = GetNode<CheckBox>("%OrderCheckBox");
+        _deleteButton = GetNode<Button>("%DeleteButton");
+
+        ToggleEntryButtons(true);
+    }
+
+    private void ToggleEntryButtons(bool disabled)
+    {
+        _editButton.Disabled = disabled;
+        _launchButton.Disabled = disabled;
+        _deleteButton.Disabled = disabled;
+    }
 }
