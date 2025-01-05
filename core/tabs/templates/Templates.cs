6712196ca68be0f64f779891be5c59109944d4a1
Jeren Raquel <jeraquel101@gmail.com>
2025-01-04 23:25:12 -0800

    updated UI

--- core/tabs/templates/Templates.cs
+++ core/tabs/templates/Templates.cs
@@ -4,15 +4,25 @@ using System;
 public partial class Templates : TabBase
 {
     private TemplateList _templateList;
+    private Label _noTagAddedLabel;
+    private HBoxContainer _tagContainerRoot;
+    private HFlowContainer _tagContainer;
     private TemplateDisplay _templateDisplay;
-    private TemplateSettings _templateSettings;
 
     public override void _Ready()
     {
         _templateList = GetNode<TemplateList>("%TemplateList");
         _templateList.GainFocus += OnNewFocus;
+
+        _noTagAddedLabel = GetNode<Label>("%NoTagLabel");
+        _tagContainerRoot = GetNode<HBoxContainer>("%TagContainerRoot");
+        _tagContainer = GetNode<HFlowContainer>("%TagContainer");
         _templateDisplay = GetNode<TemplateDisplay>("%TemplateDisplay");
-        _templateSettings = GetNode<TemplateSettings>("%TemplateSettings");
+
+        // TODO: Replace with checking if there's any tags.
+        // TODO: -- Defaults rn to there's no tags.
+        _noTagAddedLabel.Show();
+        _tagContainerRoot.Hide();
     }
 
     public override void LoadData()
